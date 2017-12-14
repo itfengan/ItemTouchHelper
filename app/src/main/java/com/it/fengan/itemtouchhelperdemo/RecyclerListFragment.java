3ff2bcfaeecf5896eddf6584e5408c6672c5e28d
fengan <fengan1102@gmail.com>
2017-12-14 16:26:22 +0800

    Initial commit

---
+++ app/src/main/java/com/it/fengan/itemtouchhelperdemo/RecyclerListFragment.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2015 Paul Burke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.it.fengan.itemtouchhelperdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.it.fengan.itemtouchhelperdemo.helper.OnStartDragListener;
+import com.it.fengan.itemtouchhelperdemo.helper.SimpleItemTouchHelperCallback;
+
+import java.util.ArrayList;
+
+
+/**
+ * @author Paul Burke (ipaulpro)
+ */
+public class RecyclerListFragment extends Fragment implements OnStartDragListener {
+
+    private ItemTouchHelper mItemTouchHelper;
+
+    public RecyclerListFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return new RecyclerView(container.getContext());
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerListAdapter adapter = new RecyclerListAdapter(getActivity(), this);
+        RecyclerView recyclerView = (RecyclerView) view;
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        SimpleItemTouchHelperCallback callback = new SimpleItemTouchHelperCallback(adapter);
+        ArrayList<Integer> pos = new ArrayList<>();
+        pos.add(0);
+        pos.add(adapter.getItemCount()-1);
+        callback.filterPosition(pos);//过滤第一个和最后一个
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+}
